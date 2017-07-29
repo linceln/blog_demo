dcd8d6b6e746a711abcedb13aeeaa262e5dfde91
linceln <lince2030@gmail.com>
2017-07-29 17:34:55 +0800

    评论管理页面

--- backend/views/comment/view.php
+++ backend/views/comment/view.php
@@ -6,38 +6,59 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model common\models\Comment */
 
-$this->title = $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Comments', 'url' => ['index']];
+$this->title = $model->post->title;
+$this->params['breadcrumbs'][] = ['label' => '评论管理', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="comment-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'template' => '<tr><th style="width: 100px">{label}</th><td>{value}</td></tr>',
+        'attributes' => [
+            'id',
+//            'status',
+            [
+                'attribute' => 'status',
+                'value' => $model->status0->name,
+            ],
+            [
+                'attribute' => 'create_time',
+                'value' => date('Y-m-d H:i:s', $model->create_time),
+            ],
+//            'userid',
+            [
+                'attribute' => 'userid',
+                'value' => $model->user->username,
+            ],
+            'email:email',
+//            'url:url',
+//            'post_id',
+//            [
+//                'attribute' => 'post_id',
+//                'value' => $model->post->title,
+//            ],
+//            'remind',
+            [
+                'attribute' => 'remind',
+                'value' => $model->remind0->name,
+            ],
+            'content:html',
+        ],
+    ]) ?>
+
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?= Html::a('更新', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('删除', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
+                'confirm' => '您确定要删除这条评论吗?',
                 'method' => 'post',
             ],
         ]) ?>
     </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'content:ntext',
-            'status',
-            'create_time:datetime',
-            'userid',
-            'email:email',
-            'url:url',
-            'post_id',
-            'remind',
-        ],
-    ]) ?>
 
 </div>
